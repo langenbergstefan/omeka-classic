c3ee606fe904653e1b957fcb80a1a1a356fb3d0c
Stefan Langenberg <stefan.langenberg.de@gmail.com>
2026-01-01 09:51:10 +0100

    Links to fonts.googleapis.com removed

--- admin/themes/default/common/login-header.php
+++ admin/themes/default/common/login-header.php
@@ -10,8 +10,7 @@
     <?php queue_css_file('layout'); ?>
     <?php queue_css_file('skeleton'); ?>
     <?php echo head_css(); ?>
-    <link href='//fonts.googleapis.com/css?family=Arvo:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-
+    
     <!-- JavaScripts -->
     <?php
     queue_js_file('vendor/modernizr');
